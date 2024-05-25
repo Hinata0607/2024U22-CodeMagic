e5deb0cb6876a31485219d26fc9277970f9462eb
haru0707tyonn <145511049+haru0707tyonn@users.noreply.github.com>
2024-05-25 17:31:33 +0900

    トップレイアウトのヘッダー追加

---
+++ frontend/src/layouts/topLayout/TopLayout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import { TopHeader } from './block';
+
+interface TopLayoutProps {
+	children: ReactNode;
+}
+
+export const TopLayout = ({ children }: TopLayoutProps) => {
+	return (
+		<>
+			<TopHeader />
+			{children}
+		</>
+	);
+};
