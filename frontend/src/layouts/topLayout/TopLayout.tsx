b09157ba2917538e2323aab90f398dcad4392d94
Hinata0607 <atahin0607@gmail.com>
2024-05-26 00:42:26 +0900

    デフォルトテーマ変更

--- frontend/src/layouts/topLayout/TopLayout.tsx
+++ frontend/src/layouts/topLayout/TopLayout.tsx
@@ -1,25 +1,24 @@
 'use client';
-import { TopHeader } from './block';
+import { TopBackground, TopHeader } from './block';
 import { TopHeaderLink } from './atom';
 import { TopLayoutProps } from '@/types';
 
 const Links = [
-	{link: "info", title: "お知らせ"},
-	{link: "intro", title: "ゲーム紹介"},
-	{link: "login", title: "ログイン"},
-]
+	{ link: 'info', title: 'お知らせ' },
+	{ link: 'intro', title: 'ゲーム紹介' },
+	{ link: 'login', title: 'ログイン' },
+];
 
 export const TopLayout = ({ children }: TopLayoutProps) => {
 	return (
 		<>
 			<TopHeader>
-				{
-					Links.map((link, index) => (
-						<TopHeaderLink key={index} link={link.link} title={link.title} />
-					))
-				}
+				{Links.map((link, index) => (
+					<TopHeaderLink key={index} link={link.link} title={link.title} />
+				))}
 			</TopHeader>
 			{children}
+			<TopBackground />
 		</>
 	);
 };
