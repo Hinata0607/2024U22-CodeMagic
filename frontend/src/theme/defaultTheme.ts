f09ee01812d1c614033d45d843d3a27e3dc89c7e
Hinata0607 <atahin0607@gmail.com>
2024-05-25 16:45:06 +0900

    MUIテーマ適用

--- frontend/src/theme/defaultTheme.ts
+++ frontend/src/theme/defaultTheme.ts
@@ -1,11 +1,10 @@
 'use client';
-import { grey } from '@mui/material/colors';
 import { createTheme } from '@mui/material/styles';
 
 export const defaultTheme = createTheme({
 	palette: {
 		background: {
-			default: grey[900],
+			default: '#000',
 		},
 	},
 });
