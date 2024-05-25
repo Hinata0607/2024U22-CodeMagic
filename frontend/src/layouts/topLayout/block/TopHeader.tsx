f45a6c9d48d7bfec3439dc3f439f8b8b19c8fa02
Hinata0607 <atahin0607@gmail.com>
2024-05-25 21:30:22 +0900

    トップヘッダー作成

--- frontend/src/layouts/topLayout/block/TopHeader.tsx
+++ frontend/src/layouts/topLayout/block/TopHeader.tsx
@@ -1,3 +1,28 @@
+import { Box } from '@mui/material';
+
 export const TopHeader = () => {
-	return <div>TopHeader</div>;
+
+	const HEIGHT = 70;
+	const MAX_HEIGHT = 30;
+
+	return (
+		<>
+			<Box
+				position={"fixed"}
+				top={0}
+				left={0}
+				width={'100vw'}
+				height={`${HEIGHT}px`}
+				maxHeight={`${MAX_HEIGHT}vh`}
+				sx={{
+					backgroundColor: '#000',
+				}}
+			></Box>
+			<Box
+				width={"100vw"}
+				height={`${HEIGHT}px`}
+				maxHeight={`${MAX_HEIGHT}vh`}
+			/>
+		</>
+	);
 };
