3481aed678e5812c2e63a4dbfc32ad6c15161670
Hinata0607 <atahin0607@gmail.com>
2024-05-26 22:44:28 +0900

    トップヘッダーレスポンシブ化

--- frontend/src/layouts/topLayout/block/TopHeader.tsx
+++ frontend/src/layouts/topLayout/block/TopHeader.tsx
@@ -1,7 +1,12 @@
 import { TopHeaderProps } from '@/types';
 import { Box } from '@mui/material';
+import { TopHeaderLogo } from '../atom';
+import { useBreakPoint } from '@/hooks';
+import { Menu } from '@mui/icons-material';
 
 export const TopHeader = ({ children }: TopHeaderProps) => {
+
+	const breakpoint = useBreakPoint();
 	const HEIGHT = 70;
 	const MAX_HEIGHT = 30;
 
@@ -13,18 +18,25 @@ export const TopHeader = ({ children }: TopHeaderProps) => {
 				top={0}
 				left={0}
 				display="flex"
-				justifyContent="start"
+				justifyContent={["xs", "sm"].includes(breakpoint) ? "space-between" : "start"}
 				alignItems="center"
 				gap="50px"
 				width="100vw"
 				height={`${HEIGHT}px`}
 				maxHeight={`${MAX_HEIGHT}vh`}
-				p="0 50px"
+				p={["xs", "sm"].includes(breakpoint) ? "0 25px" : "0 50px"}
 				sx={{
 					backgroundColor: 'layout.topLayout.primary',
 				}}
 			>
-				{children}
+				<TopHeaderLogo />
+				<Box
+					display="flex"
+					alignItems="center"
+					gap="50px"
+				>
+					{["xs", "sm"].includes(breakpoint) ? <Menu /> : children}
+				</Box>
 			</Box>
 			<Box width="100vw" height={`${HEIGHT}px`} maxHeight={`${MAX_HEIGHT}vh`} />
 		</>
